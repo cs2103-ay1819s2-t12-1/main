1fbca9731a5b7ec498ab447e0bafa9bf5697d202
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-01 03:02:52 +0800

    made it adaptable to work

--- src/main/java/seedu/address/model/bill/Bill.java
+++ src/main/java/seedu/address/model/bill/Bill.java
@@ -9,15 +9,13 @@ public class Bill {
 
     private Customer customer;
     private int amount_paid;
-    private Optional<List<Booking>> bookings;
-    private Optional<List<Reservation>> reservations;
 
-    public Bill(Customer customer, int amount_paid, Optional<List<Booking>> bookings, Optional<List<Reservation>> reservations){
+
+    public Bill(Customer customer, int amount_paid ){
 
         this.customer = customer;
         this.amount_paid = amount_paid;
-        this.bookings = bookings;
-        this.reservations = reservations;
+
     }
 
     public Customer getCustomer(){
@@ -28,13 +26,6 @@ public class Bill {
         return amount_paid;
     }
 
-    public Optional<List<Booking>> getBookings() {
-        return bookings;
-    }
-
-    public Optional<List<Reservation>> getReservations() {
-        return reservations;
-    }
 
     @Override
     public String toString(){
