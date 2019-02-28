6eed5d2d0cc4853935634b7b50be6d9e8702e928
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-01 03:28:39 +0800

    Added Javadoc comments

--- src/main/java/seedu/address/model/bill/Bill.java
+++ src/main/java/seedu/address/model/bill/Bill.java
@@ -2,7 +2,9 @@ package seedu.address.model.bill;
 
 import seedu.address.model.customer.Customer;
 
-
+/**
+ * Represents a Customer's bill in the system.
+ */
 public class Bill {
 
     private Customer customer;
@@ -21,10 +23,16 @@ public class Bill {
 
     }
 
+    /**
+     * Returns the customer whose bill is to be calculated.
+     */
     public Customer getCustomer() {
         return customer;
     }
 
+    /**
+     * Returns the amount already paid by the customer.
+     */
     public int getAmountPaid() {
         return amountPaid;
     }
