7fb0883fd9533f1422b77cd62ea76543fa4d62f2
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-02 04:36:50 +0800

    Still correcting errors

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -186,8 +186,8 @@ public class EditCommandParserTest {
             + PHONE_DESC_BOB + ADDRESS_DESC_BOB + EMAIL_DESC_BOB + TAG_DESC_HUSBAND;
 
         EditCustomerDescriptor descriptor = new EditCustomerDescriptorBuilder().withPhone(VALID_PHONE_BOB)
-            .withEmail(VALID_EMAIL_BOB).withId(VALID_ID_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_FRIEND, VALID_TAG_HUSBAND)
-            .build();
+            .withEmail(VALID_EMAIL_BOB).withId(VALID_ID_BOB).withAddress(VALID_ADDRESS_BOB)
+            .withTags(VALID_TAG_FRIEND, VALID_TAG_HUSBAND).build();
         EditCommand expectedCommand = new EditCommand(targetIndex, descriptor);
 
         assertParseSuccess(parser, userInput, expectedCommand);
